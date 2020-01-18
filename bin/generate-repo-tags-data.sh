59a47d013bba2b7cfba502cfdabb846ae5219cde
Danny Berger <dpb587@gmail.com>
2020-01-18 12:06:30 -0800

    fix indentation

--- bin/generate-repo-tags-data.sh
+++ bin/generate-repo-tags-data.sh
@@ -12,6 +12,6 @@ cd "$repo"
 (
   echo 'dates:'
   for tag in $( git tag -l ); do
-    echo "$tag: $( git log -n1 --pretty='format:%ai' "$tag" )"
+    echo "  $tag: $( git log -n1 --pretty='format:%ai' "$tag" )"
   done
 ) > "$sitedir/data/repo/tags.yml"
